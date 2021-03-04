7611f1412d615be5a3bbba649e1d59dd62670c48
张平 <zhangping02@qiyi.com>
2021-03-04 21:13:17 +0800

    copyright

--- app/src/main/java/com/example/androiddevchallenge/CountDownView.kt
+++ app/src/main/java/com/example/androiddevchallenge/CountDownView.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.compose.foundation.Canvas
@@ -10,9 +25,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,29 +56,37 @@ fun MyApp(viewModel: CountdownViewModel) {
             modifier = Modifier.fillMaxSize()
         ) {
 
-            Button(onClick = {
-                if (isPause) {
-                    viewModel.restartCount()
-                } else {
-                    viewModel.startCount(3 * 60 * 1000)
-                }
-                isPause = false
-                isStart = true
-            }, enabled = isStart.not()) {
+            Button(
+                onClick = {
+                    if (isPause) {
+                        viewModel.restartCount()
+                    } else {
+                        viewModel.startCount(3 * 60 * 1000)
+                    }
+                    isPause = false
+                    isStart = true
+                },
+                enabled = isStart.not()
+            ) {
                 Text(text = if (isPause) "RESTART" else "START")
             }
-            Button(enabled = isStart, onClick = {
-                isPause = true
-                isStart = false
-                viewModel.pauseCount()
-            }) {
+            Button(
+                enabled = isStart,
+                onClick = {
+                    isPause = true
+                    isStart = false
+                    viewModel.pauseCount()
+                }
+            ) {
                 Text(text = "PAUSE")
             }
-            Button(onClick = {
-                isPause = false
-                isStart = false
-                viewModel.resetCount()
-            }) {
+            Button(
+                onClick = {
+                    isPause = false
+                    isStart = false
+                    viewModel.resetCount()
+                }
+            ) {
                 Text(text = "RESET")
             }
         }
--- app/src/main/java/com/example/androiddevchallenge/CountdownViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/CountdownViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import android.os.CountDownTimer
@@ -26,30 +41,38 @@ class CountdownViewModel : ViewModel() {
         _isPause = false
         countDownTimer?.cancel()
 
-        countDownTimer = obtainCountDownTimer(mTime, onTick = { millisUntilFinished ->
-            Log.e("CountDownTimer", "onTick time:$millisUntilFinished")
-            _currentTime = millisUntilFinished
-            _percent = 1.0f - (1.0f * millisUntilFinished) / mTime
-        }, onFinish = {
-            Log.e("CountDownTimer", "onFinish--")
-            _currentTime = 0
-            _percent = 1f
-        })
+        countDownTimer = obtainCountDownTimer(
+            mTime,
+            onTick = { millisUntilFinished ->
+                Log.e("CountDownTimer", "onTick time:$millisUntilFinished")
+                _currentTime = millisUntilFinished
+                _percent = 1.0f - (1.0f * millisUntilFinished) / mTime
+            },
+            onFinish = {
+                Log.e("CountDownTimer", "onFinish--")
+                _currentTime = 0
+                _percent = 1f
+            }
+        )
 
         countDownTimer?.start()
     }
 
     fun restartCount() {
         val tempTime = _currentTime
-        countDownTimer = obtainCountDownTimer(tempTime, onTick = { millisUntilFinished ->
-            Log.e("CountDownTimer", "onTick time:$millisUntilFinished")
-            _currentTime = millisUntilFinished
-            _percent = 1.0f - (1.0f * millisUntilFinished) / mTime
-        }, onFinish = {
-            Log.e("CountDownTimer", "onFinish--")
-            _currentTime = 0
-            _percent = 1f
-        })
+        countDownTimer = obtainCountDownTimer(
+            tempTime,
+            onTick = { millisUntilFinished ->
+                Log.e("CountDownTimer", "onTick time:$millisUntilFinished")
+                _currentTime = millisUntilFinished
+                _percent = 1.0f - (1.0f * millisUntilFinished) / mTime
+            },
+            onFinish = {
+                Log.e("CountDownTimer", "onFinish--")
+                _currentTime = 0
+                _percent = 1f
+            }
+        )
         countDownTimer?.start()
         _isPause = false
     }
@@ -77,14 +100,12 @@ class CountdownViewModel : ViewModel() {
                 else -> time.toString()
             }
 
-
         val time =
             getFormat(hours % 24) + ":" + getFormat(minutes % 60) + ":" + getFormat(
                 seconds % 60
             )
         return time
     }
-
 
     private fun obtainCountDownTimer(
         time: Long,
